b43ff2649d97e35299509638754e8e2edba12e54
Yasser Souri <yassersouri@gmail.com>
2010-10-30 23:08:37 +0330

    users database created and added to sql/createdatabase.sql

---
+++ sql/createdatabase.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 3.0.1.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 30, 2010 at 07:37 PM
+-- Server version: 5.1.30
+-- PHP Version: 5.2.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `hekmat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sokhanrans`
+--
+
+CREATE TABLE `sokhanrans` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `Description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `sokhanrans`
+--
+
+INSERT INTO `sokhanrans` (`id`, `name`, `Description`) VALUES
+(4, 'Ghasemian', 'Ghasemian Ghasemian\r\nGhasemian\r\nGhasemian\r\nGhasemian\r\nGhasemianGhasemian'),
+(5, 'Panahian', 'Panahian\r\nPanahian\r\nPanahian\r\nPanahian\r\n'),
+(6, 'Amjad', 'aslfjasldfjlasdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(500) NOT NULL,
+  `password` varchar(40) NOT NULL COMMENT 'md5ed',
+  `firstname` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `lastname` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `isAdmin` int(1) NOT NULL,
+  `isSokhanran` int(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `users`
+--
+
