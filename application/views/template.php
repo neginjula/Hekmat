262f2f360407ea1b30f89d283bd1e5027f0f0a34
Yasser Souri <yassersouri@gmail.com>
2010-10-30 22:32:44 +0330

    added a template for loading views in views/template.php this maked lots of code reuse in creating views

---
+++ application/views/template.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="fa">
+	<head>
+		<meta charset="utf-8" />
+		<title><?php echo $title;?></tilte> <!-- title will be passed to this view in variable $title -->
+		<link rel="stylesheet" href="<?php echo base_url;?>style/default.css" type="text/css" media="screen" title="default css" charset="utf-8"> <!-- we use php to specify the default css address -->
+	</head>
+	
+	<body>
+		<?php $this->load->view($mainView);?> <!-- from here forward we use another view wich is passed in a variable called $mainView -->
+	</body>
+</html>
